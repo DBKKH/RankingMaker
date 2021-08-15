2d9abe8be9f91e4ddca1352c2fe7e451267aaa35
hideki.koike <hdkkik@gmail.com>
2021-08-15 17:13:27 +0900

    add stocked param

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 class Page {
-    constructor(title, url, userName, postedTime, likeNum = 0, commentNum = 0, openedBag = 0, jpycNum = 0, doggodNum = 0) {
+    constructor(title, url, userName, postedTime, likeNum = 0, stockedNum = 0, commentNum = 0, openedBag = 0, jpycNum = 0, doggodNum = 0) {
         this.title = title;
         this.url = url;
         this.userName = userName;
         this.postedTime = postedTime;
         this.likeNum = likeNum;
+        this.stockedNum = stockedNum;
         this.commentNum = commentNum;
         this.openedBag = openedBag;
         this.jpycNum = jpycNum;
@@ -12,41 +13,95 @@ class Page {
     }
 }
 
-const minitue = '分前';
-const hours = '時間前';
-const days = '日前'
 
+var minitue = '分前';
+var hours = '時間前';
+var days = '日前'
 var lastDay;
-var lastPageNum = 0;
 var fileName = 'ranking.csv';
 var pageList = [];
 var postedTime;
-
 var page;
 var posts;
-
 var host_url = location.host;
 
-
-if (host_url === "hide.ac") {
-    Init();
-}
+if (host_url === "hide.ac") { Init(); }
 
 function Init() {
     InitDate();
 
     // Add colum names
-    pageList.push(['title', 'url', 'userName', 'postedTime', 'likeNum', 'comments', 'openedBag', 'jpyc', 'doggod']);
+    pageList.push(['title', 'url', 'userName', 'postedTime', 'likeNum', 'stockedNum', 'comments', 'openedBag', 'jpyc', 'doggod']);
 
-    GetPosts();
+    LoadPages();
 }
 
 function InitDate(){
     let dt = new Date();
-    let date = dt.getDate() - 1;
-    lastDay = date * 24;
+    lastDay = dt.getDate();
 }
 
+var postTimes;
+var lastTime;
+
+async function LoadPages(){
+    while(true){
+        if(lastTime > lastDay) {
+            GetPosts();
+            console.log('End');
+            break;
+        }
+        else{
+            await CheckPostTiem().then(result =>{
+                console.log(result);
+            });
+
+            // var promise =new Promise((resolve, reject)=>{
+            //     CheckPostTiem();
+            //     console.log(promise);
+            //     resolve();
+            // })
+            // .then(()=>{
+            //     console.log(promise + " continued and then");
+            //     // continue;
+            // })
+            // .catch(()=>{
+            //     console.log(promise + " catched");
+            // });
+        }
+    }
+}
+
+function CheckPostTiem(){
+    return new Promise((resolve, reject)=>{
+        postTimes = [...document.querySelectorAll('.css-1pysja1')];
+        var finalday = postTimes[postTimes.length - 1].textContent;
+        
+        if (finalday.includes(days)) {
+            lastTime = +(finalday.replace(days, ''));
+        }
+        
+        NextPage();
+        resolve();
+    });
+}
+
+function NextPage() {
+    console.log('next');
+    try {
+        ClickNextPage();
+    }
+    catch (e) {
+        console.error(e);
+        ClickNextPage();
+    }
+}
+
+function ClickNextPage(){
+    var ele1 = document.getElementsByClassName("css-wmz95n")[0];
+    console.log(ele1);
+    ele1.click();
+}
 
 function GetPosts() {
     posts = [...document.querySelectorAll('.css-rpl3vx')];
@@ -55,8 +110,6 @@ function GetPosts() {
 
 function AddPosts() {
     posts.forEach(function (elm, index) {
-
-        if (index < lastPageNum) return;
 
         let title = elm.querySelector('.css-1pysja1').textContent;
 
@@ -72,22 +125,20 @@ function AddPosts() {
 
         // change to hour unit
         if (postedTimeString.includes(minitue)) {
-            postedTime = +(postedTimeString.replace(minitue, '')) / 60;
+            postedTime = +(postedTimeString.replace(minitue, '')) / (60 * 24);
         }
         else if (postedTimeString.includes(hours)) {
-            postedTime = +(postedTimeString.replace(hours, ''));
+            postedTime = +(postedTimeString.replace(hours, '')) / 24;
         }
         else if (postedTimeString.includes(days)) {
-            postedTime = +(postedTimeString.replace(days, '')) * 24;
+            postedTime = +(postedTimeString.replace(days, ''));
         }
         else {
             console.error('There is not posted time.')
         }
 
-        if (postedTime > lastDay) return; // should be break
-
-
         let likeNum = +(elm.querySelector('.css-1fyivyj') || { textContent: 0 }).textContent;
+        let stockedNum = +(elm.querySelector('.css-9ludpm') || { textContent: 0 }).textContent;
         let commentsNum = +(elm.querySelector('.css-px5w92') || { textContent: 0 }).textContent;
         let openedBag = +(elm.querySelector('.css-1gi2xsa') || { textContent: 0 }).textContent;
 
@@ -97,46 +148,24 @@ function AddPosts() {
         if (values.length == 2) {
             let jpycValue = +(values[0].textContent);
             var doggodValue = +(values[1].textContent);
-            page = new Page(title, url, userName, postedTime, likeNum, commentsNum, openedBag, jpycValue, doggodValue);
+            page = new Page(title, url, userName, postedTime, likeNum, stockedNum, commentsNum, openedBag, jpycValue, doggodValue);
         }
         // has only jpyc
         else if (values.length == 1) {
             let jpycValue = +(values[0]).textContent;
-            page = new Page(title, url, userName, postedTime, likeNum, commentsNum, openedBag, jpycValue);
+            page = new Page(title, url, userName, postedTime, likeNum, stockedNum, commentsNum, openedBag, jpycValue);
         }
         // has no token
         else {
-            page = new Page(title, url, userName, postedTime, likeNum, commentsNum, openedBag);
+            page = new Page(title, url, userName, postedTime, likeNum, stockedNum, commentsNum, openedBag);
         }
 
-        pageList.push([page.title, page.url, page.userName, page.postedTime, page.likeNum, page.commentNum, page.openedBag, page.jpycNum, page.doggodNum]);
+        pageList.push([page.title, page.url, page.userName, page.postedTime, page.likeNum, page.stockedNum, page.commentNum, page.openedBag, page.jpycNum, page.doggodNum]);
 
     });
 
-    lastPageNum = posts.length;
-
-    if (page.postedTime < lastDay) {
-        NextPage();
-        setTimeout(() => {
-            GetPosts();
-        }, 500);
-    }
-    else {
-        console.log(pageList);
-        CreateAndDownloadCsv(pageList);
-    }
-}
-
-
-var nextPageButtonElement = "css-wmz95n";
-
-function NextPage() {
-    try {
-        const ele1 = document.getElementsByClassName(nextPageButtonElement);
-        ele1[0].click();
-    } catch (e) {
-        console.error(e);
-    }
+    console.log(pageList);
+    CreateAndDownloadCsv(pageList);
 }
 
 
@@ -152,25 +181,6 @@ function CreateAndDownloadCsv(array) {
     downloadLink.href = URL.createObjectURL(blob);
     downloadLink.dataset.downloadurl = ['text/plain', downloadLink.download, downloadLink.href].join(':');
     downloadLink.click();
-}
-
-
-
-// ---loader-------------------------------------------------------------
-
-function OnBottom() {
-
-    window.onscroll = function () {
-
-        let windowH = document.body.clientHeight;
-        let bodyH = document.documentElement.scrollHeight;
-        let scrollErea = bodyH - windowH;
-        let top = document.documentElement.scrollTop;
-
-        if (scrollErea <= top - 50) {
-            NextPage();
-        }
-    }
 }
 
 
